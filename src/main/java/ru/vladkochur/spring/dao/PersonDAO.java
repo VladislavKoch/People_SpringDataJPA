9749503fc0356ea22cbc864a8836e34e777972ff
Vladislav Kochurovskiy <kochurovsckij@yandex.ru>
2024-03-13 15:05:01 +0300

    First commit

---
+++ src/main/java/ru/vladkochur/spring/dao/PersonDAO.java
@@ -0,0 +1,49 @@
+package ru.vladkochur.spring.dao;
+
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+import ru.vladkochur.spring.models.Person;
+
+import java.util.List;
+import java.util.Optional;
+
+@Component
+public class PersonDAO {
+
+    private final SessionFactory sessionFactory;
+
+    @Autowired
+    public PersonDAO(SessionFactory sessionFactory) {
+        this.sessionFactory = sessionFactory;
+    }
+
+    @Transactional(readOnly = true)
+    public List<Person> index() {
+        Session session = sessionFactory.getCurrentSession();
+        List<Person> people = session.createQuery("from Person", Person.class).getResultList();
+        return people;
+    }
+
+    public Person show(int id) {
+        return null;
+    }
+
+    public Optional<Person> show(String email) {
+        return null;
+    }
+
+    public void save(Person person) {
+
+    }
+
+    public void update(int id, Person updatedPerson) {
+
+    }
+
+    public void delete(int id) {
+    }
+}
---
+++ src/main/java/ru/vladkochur/spring/models/Person.java
@@ -0,0 +1,87 @@
+package ru.vladkochur.spring.models;
+
+
+
+import javax.persistence.*;
+import javax.validation.constraints.*;
+
+@Entity
+@Table(name = "Person")
+public class Person {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+
+
+    @Column(name = "name")
+    @NotEmpty(message = "You must write name")
+    @Size(min = 2, max = 30, message = "Name length less than 2 or larger than 30")
+    private String name;
+
+
+    @Column(name = "age")
+    @Min(value = 0, message = "Age must be 1+")
+    private int age;
+
+    @Column(name = "email")
+    @NotEmpty(message = "You must write email")
+    @Email(message = "Email is not correct")
+    private String email;
+
+    @Column(name = "address")
+    @Pattern(regexp = "^(.*?)\\s*,\\s*(.*?)\\s*,\\s*(\\d{6})$", message = "Wright Country, City, Index like 000000")
+    private String address;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public Person(String name, int age, String email, String address) {
+        this.name = name;
+        this.age = age;
+        this.email = email;
+        this.address = address;
+    }
+
+    public Person() {
+    }
+
+
+}
